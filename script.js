75a6b46953b32478ac69c1e26b79eb6b0228e462
Andrew Lowe <doodlefolks@gmail.com>
2016-02-28 15:44:28 -0800

    Add function to select images based on grid size

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 (function() {
+  'use strict';
+
   var hideButtons = function() {
     var buttons = document.getElementsByClassName('game-select');
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].style.display = 'none';
     }
   }
+  var getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  var pickImgSet = function(gridSize) {
+    var availableImages = imageNames;
+    var selectedImages = [];
+    var pick;
 
+    for (var i = 0; i < gridSize * gridSize / 2; i++) {
+      selectedImages.push(availableImages.splice(getRandomInt(0, availableImages.length - 1), 1));
+    }
+    debugger;
+    console.log(availableImages);
+    console.log(selectedImages);
+    return selectedImages;
+  }
+
+  var imageNames = ['pig.jpeg', 'mt_rainier.jpg', 'tree.jpg', 'orange.jpg', 'apple.jpg', 'bear.jpg', 'cat.jpeg', 'fish.JPG'];
   var fourByFour = document.getElementById('four-by-four');
   var sixBySix = document.getElementById('six-by-six');
   var eightByEight = document.getElementById('eight-by-eight');
 
   fourByFour.addEventListener('click', function() {
-    var row, gameTile, imageNames, tileImage;
+    var row, gameTile, tileImage;
+    var selectedImages = pickImgSet(4);
     var main = document.getElementsByTagName('main')[0];
 
-    debugger;
-    console.log(main);
     main.style.flexDirection = 'column';
     hideButtons();
-    imageNames = ['pig.jpeg', 'mt_rainier.jpg', 'tree.jpg', 'orange.jpg'];
     for (var i = 0; i < 4; i++) {
       row = document.createElement('div');
       row.className = 'row';
@@ -26,7 +43,7 @@
         gameTile = document.createElement('div');
         gameTile.className = 'game-tile';
         tileImage = document.createElement('img');
-        tileImage.src = 'images/' + imageNames[j];
+        tileImage.src = 'images/' + selectedImages[j];
         gameTile.appendChild(tileImage);
         row.appendChild(gameTile);
       }
