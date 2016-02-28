c13685bdb67c2b8609f6b9e3b2e2a03cb52b9497
Andrew Lowe <doodlefolks@gmail.com>
2016-02-28 09:52:46 -0800

    Set up 4x4 w/ temp images

--- script.js
+++ script.js
@@ -11,8 +11,26 @@
   var eightByEight = document.getElementById('eight-by-eight');
 
   fourByFour.addEventListener('click', function() {
-    hideButtons();
-    var gameTile = document.createElement('div');
+    var row, gameTile, imageNames, tileImage;
+    var main = document.getElementsByTagName('main')[0];
 
+    debugger;
+    console.log(main);
+    main.style.flexDirection = 'column';
+    hideButtons();
+    imageNames = ['pig.jpeg', 'mt_rainier.jpg', 'tree.jpg', 'orange.jpg'];
+    for (var i = 0; i < 4; i++) {
+      row = document.createElement('div');
+      row.className = 'row';
+      for (var j = 0; j < 4; j++) {
+        gameTile = document.createElement('div');
+        gameTile.className = 'game-tile';
+        tileImage = document.createElement('img');
+        tileImage.src = 'images/' + imageNames[j];
+        gameTile.appendChild(tileImage);
+        row.appendChild(gameTile);
+      }
+      main.appendChild(row);
+    }
   });
 })();
