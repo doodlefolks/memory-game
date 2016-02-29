e6c65a636f1c225a6e9770c9159f15a991d00f8b
Andrew Lowe <doodlefolks@gmail.com>
2016-02-28 19:35:37 -0800

    Setup tile click functionality

--- script.js
+++ script.js
@@ -35,11 +35,15 @@
       row = document.createElement('div');
       row.className = 'row';
       for (var j = 0; j < gridSize; j++) {
-        gameTile = document.createElement('div');
-        gameTile.className = 'game-tile';
-        tileImage = document.createElement('img');
         var tempIndex = getRandomInt(0, gameTileImages.length - 1);
+        gameTile = document.createElement('div');
+        gameTile.className = 'game-tile game-tile-hidden';
+        gameTile.addEventListener('click', clickTile);
+
+        tileImage = document.createElement('img');
         tileImage.src = 'images/' + gameTileImages[tempIndex];
+        tileImage.className = 'game-tile-image'
+
         gameTileImages.splice(tempIndex, 1);
         gameTile.appendChild(tileImage);
         row.appendChild(gameTile);
@@ -47,13 +51,41 @@
       main.appendChild(row);
     }
   };
+  var clickTile = function() {
+    var clickedImg = this.getElementsByTagName('img')[0];
+    clickedImg.style.display = 'inline-block';
+    if (prevTileClicked) {
+      if (clickedImg !== prevClickedTileImg && clickedImg.src === prevClickedTileImg.src) {
+        alert('match');
+        this.className = 'game-tile game-tile-shown';
+        this.removeEventListener('click', clickTile);
+        prevClickedTileImg.parentNode.className = 'game-tile game-tile-shown';
+        prevClickedTileImg.parentNode.removeEventListener('click', clickTile);
+      } else {
+        imgPointerForDelay = prevClickedTileImg;
+        window.setTimeout(function() {
+          clickedImg.style.display = 'none';
+          imgPointerForDelay.style.display = 'none';
+        }, 500);
+      }
+      prevClickedTileImg = null;
+      prevTileClicked = false;
+    } else {
+      prevClickedTileImg = clickedImg;
+      prevTileClicked = true;
+    }
+  };
 
   var imageNames = ['pig.jpeg', 'mt_rainier.jpg', 'tree.jpg', 'orange.jpg', 'apple.jpg', 'bear.jpg', 'cat.jpeg', 'fish.JPG', 'bowling_ball.jpg', 'pineapple.jpg', 'waldo.jpg', 'rat.jpg', 'scooby.jpg', 'boat.jpg', 'dog.jpg', 'beer.jpg', 'arch.jpg', 'space_needle.jpg'];
   var fourByFour = document.getElementById('four-by-four');
   var sixBySix = document.getElementById('six-by-six');
   var eightByEight = document.getElementById('eight-by-eight');
+  var prevTileClicked = false;
+  var prevClickedTileImg, imgPointerForDelay;
 
   fourByFour.addEventListener('click', function() {
+    var gameTiles;
+
     hideButtons();
     setupImages(4);
   });
